a7e82764edd74bae47e7116af8b5c05ccfb1ed09
Jim Diroff II <jim@dirofftech.com>
2023-08-05 22:53:24 -0400

    Begin initial rooting attempts for basic levels

--- README.md
+++ README.md
@@ -28,3 +28,9 @@ Starting a new game and connecting the web socket copies over the `template.js` 
 - [x] Create a script that will generate a list of servers to hack.
 
 `scanToFile.js` scans the local network for servers, and stores the hostnames to a JSON-formatted file called `servers.js`. Files created in-game are not copied back to our local machine. We don't need them to develop our scripts, but they might be useful for reference and debugging. Download a file from the game with the `download` command, and I've been storing them in the `download` folder for quick access. I don't want these dymamically created files to end up in the dist, so ensure they do not get copied to `src`.
+
+Now we have our list of servers, we can use it to connect and root them.
+
+- [x] Create a script that will connect to a server and root it.
+
+`conn.js` will connect to a server and root it. The root process can only be done if we meet the hacking level, which will continuously increase. As our hacking level increases, we want to ensure we gain access to newly available servers. Running `conn.js` continually will ensure we are always connected to the highest level server we can access. We will have to later modify our scans to append to the `servers.js` file to ensure we access new servers. We also want to skip servers that we have already rooted, or too high of a hacking level.
---
+++ src/conn.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-constant-condition */
+/**
+ * @param {NS} ns
+ *
+ * This script connects to servers and roots them.
+ */
+
+export async function main(ns) {
+	if (!ns.fileExists("servers.js")) {
+		ns.tprint("servers.js not found. Run scanToFile.js first.");
+	}
+
+	ns.tprint("servers.js found. Scanning servers...");
+
+	while (true) {
+		const serverData = ns.read("servers.js");
+		const servers = JSON.parse(serverData);
+
+		const excludedServers = ["home", "darkweb"];
+
+		for (const server of servers) {
+			ns.tprint("checking: " + server);
+
+			/* exclude unique servers */
+			if (excludedServers.includes(server)) {
+				ns.tprint("excluded unique: " + server);
+				continue;
+			}
+
+			/* exclude servers that are already rooted */
+			if (ns.hasRootAccess(server)) {
+				ns.tprint("excluded nuked: " + server);
+				continue;
+			}
+
+			/* exclude servers that are too high level */
+			if (ns.getHackingLevel() < ns.getServerRequiredHackingLevel(server)) {
+				ns.tprint("excluded hack level: " + server);
+				continue;
+			}
+
+			/* exclude servers with too many ports */
+			if (ns.getServerNumPortsRequired(server) > 0) {
+				ns.tprint("excluded ports: " + server);
+				continue;
+			}
+
+			ns.tprint("nuking: " + server);
+			ns.nuke(server);
+			ns.tprint("nuked: " + server);
+		}
+
+		// sleep for a cycle
+		const sleepTime = 10000; // 10 seconds
+		ns.tprint("sleeping for " + sleepTime + "ms");
+		await ns.sleep(sleepTime);
+	}
+}
